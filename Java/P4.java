e7f6ccf4fd32358a7102dc5379aa03b9f3e1b1b1
Ashutosh Yadav <ashutoshyadav9580@gmail.com>
2026-06-26 16:32:54 +0530

    Explore about Scanner class

---
+++ Java/P4.java
@@ -0,0 +1,13 @@
+// This is single line comment 
+/*
+This is multiline
+comment
+*/
+
+public class P4 {
+        public static void main(String[] args){
+              System.out.println("This is 1st line");
+              System.out.println("This is 1st line");
+
+   }
+}
