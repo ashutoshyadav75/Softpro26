40d34f739beb2600348140a6a4a18e53fde81617
Ashutosh Yadav <ashutoshyadav9580@gmail.com>
2026-06-30 11:23:26 +0530

    Java Operator

--- P36.java
+++ P36.java
@@ -4,7 +4,5 @@ public class P36{
        public static void main(String[] args){
                    System.out.println("Result without Brackets :" + (5+5/2));
                    System.out.println("Result without Brackets :" + ((5+5)/2));
-}
-}
   
 
