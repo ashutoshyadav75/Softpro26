29ccfce30cb742c099ac4929900a3857cb6d44d6
Ashutosh Yadav <ashutoshyadav9580@gmail.com>
2026-07-03 09:43:02 +0530

    Odd and Even by If

---
+++ Java/P57.java
@@ -0,0 +1,21 @@
+
+
+
+
+
+import java.util.Scanner;
+public class P57{
+       public static void main(String[] args){
+                System.out.print("Enter the number:");
+                  Scanner sc = new Scanner(System.in);
+                   int n = sc.nextInt();
+                       if(n>=18){
+                      System.out.print("You are eligible for Vote");
+                       }
+                     else{
+                         System.out.print("You are not  eligible for Vote");
+                      }
+}
+}
+
+
