922a902fcba00e6c8226b40cf4309aa01cc9143d
Ashutosh Yadav <ashutoshyadav9580@gmail.com>
2026-07-05 10:36:28 +0530

    All even no. with continue

---
+++ Java/P78.java
@@ -0,0 +1,15 @@
+// WAP all even number which is not multiply of 10 with continue
+
+public class P79{
+         public static void main(String[] args){
+               for(int i = 1 ; i<=100 ; i++){
+                 if(i%2==0){
+                             if(i%10 == 0){
+                                   continue; 
+                              }
+               System.out.print(" ::: " +i); 
+                       
+}
+}
+}
+}
