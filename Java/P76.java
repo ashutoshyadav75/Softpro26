812cf7c18acf3ce2e4f391c29d824086b80e6011
Ashutosh Yadav <ashutoshyadav9580@gmail.com>
2026-07-05 11:18:57 +0530

    Declaration of arrays

---
+++ Java/P76.java
@@ -0,0 +1,11 @@
+public class P75{
+         public static void main(String[] args){
+          
+                   
+             for(int i =0 ; i<10 ; i++){
+               System.out.print("\nt i=>" + i);
+                 continue;
+           System.out.print("This code won't Execute");
+}
+}
+}
---
+++ Java/P81.java
@@ -0,0 +1,50 @@
+//WAP in java to working with Array
+
+public class P81{
+         public static void main(String[] args){
+
+                                int[] arr = { 10, 20, 30, 40};
+                                     int n = arr.length;
+                                      System.out.println("Array size:"  +n);
+                    
+                                       System.out.println("==================================");
+                                       System.out.println("1st Element:"  +arr[0]);
+                                        System.out.println("2nd  Element:"  +arr[1]);
+                                      System.out.println("3rd Element:"  +arr[2]);
+                                              //System.out.println("Dynamic Element:"  + arr[n]);
+                                   for(int i = 0 ; i<n ; i++){
+                                 System.out.println("Array Element:"  +arr[i]);
+}
+
+           
+                         char[] ch = {'A' , 'B' , 'C' , 'D'  , 'E'};
+              
+                                    int m = ch.length;
+                                      System.out.println("Array size:"  +m);
+                    
+                                       System.out.println("==================================");
+                                          System.out.println("1st Element:"  +ch[0]);
+                                       System.out.println("2nd Element:"  +ch[1]);
+                                        System.out.println("3rd  Element:"  +ch[2]);
+                                      System.out.println("4th Element:"  +ch[3]);
+                                              //System.out.println("Dynamic Element:"  + ch(m));
+                                   for(int i = 0 ; i<m ; i++){
+                                 System.out.println("Array Element:"  +ch[i]);
+}
+                    
+                              String[] st = {"Ashutosh " , "Khushi" , "Nitin" , "Chandhan"};
+                                               int o = ch.length;
+                                      System.out.println("Array size:"  +o);
+                    
+                                       System.out.println("==================================");
+                                       System.out.println("1st Element:"  +st[0]);
+                                        System.out.println("2nd  Element:"  +st[2]);
+                                      System.out.println("3rd Element:"  +st[3]);
+                                              //System.out.println("Dynamic Element:"  + st(o));
+                                   for(int i = 0 ; i<m ; i++){
+                                 System.out.println("Array Element:"  +st[i]);
+}
+
+
+}
+}
