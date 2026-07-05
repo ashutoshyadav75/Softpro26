40eb9f951d83fc2970590ff0920e397f64a5dc28
Ashutosh Yadav <ashutoshyadav9580@gmail.com>
2026-07-05 11:44:06 +0530

    For each loop

---
+++ Java/P80.java
@@ -0,0 +1,15 @@
+// WAP all even number which is not multiply of 10
+
+public class P79{
+         public static void main(String[] args){
+               for(int i = 1 ; i<=100 ; i++){
+                 if(i%2==0){
+                             if(i%10 != 0){
+                                    System.out.print(" ::: " +i); 
+                              }
+               
+                       
+}
+}
+}
+}
---
+++ Java/P82.java
@@ -0,0 +1,10 @@
+ // For each loop
+
+ public class P82{
+             public static void main(String[] args){
+                       int[] arr = {10 , 34, 35, 65, 39, 53};
+                              for(int ele : arr){
+                           System.out.println("Elements:" +ele);
+    }
+}
+}
