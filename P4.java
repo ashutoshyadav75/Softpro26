b790ea92149c6dc666d1ff52d6c4acff6385de51
ashutoshyadav75 <ashutoshyadav9580@gmail.com>
2026-07-09 19:58:03 +0530

    Add files via upload

--- P4.java
+++ P4.java
@@ -1,13 +1,29 @@
-// This is single line comment 
-/*
-This is multiline
-comment
-*/
+class TV {
+      String brand = "Sony";
+      String type = "oled"; 
+       int price = 45;
+      int size = 65;
 
-public class P4 {
-        public static void main(String[] args){
-              System.out.println("This is 1st line");
-              System.out.println("This is 1st line");
+    public static void on(){
+    System.out.println(".....Tv is on....");
+    System.out.println("...............Sony.............");
+}
 
-   }
+public static void off(){
+    System.out.println("..............Tv is off.............");
+}
+}
+
+   public class P4{
+       public static void main(String[] args){
+  System.out.println(".....Main method is running....");
+             (new TV()).on();   // Anonymous
+      
+
+
+         //Invalid
+       TV.on();
+        TV.off();
+    
+}
 }
